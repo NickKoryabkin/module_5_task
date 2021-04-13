cb604e932fa82040bd68443e74baf04a5d5181da
NickKoryabkin <79140503+NickKoryabkin@users.noreply.github.com>
2021-04-13 22:48:15 +0300

    1

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+print("Исходные данные")
 f = open("1.txt", "r")
-print(f.readline())
-f.close()
+print(f.read())
+
+
+
+FIND = "tmp"
+INFILE = "1.txt"
+OUTFILE = "2.txt"
+ENC = "utf-8"
+print("Данные после выполнения задачи")
+with open(INFILE, encoding=ENC) as infile, open(OUTFILE, "w", encoding=ENC) as outfile:
+    for line in infile:
+        if FIND not in line:
+            outfile.write(line)
+f = open("2.txt", "r")
+print(f.read())
